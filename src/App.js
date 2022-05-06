cca627692e3d44c814f8b5cc645809b4ad440cf1
Burak Kucukparmaksiz <burakkp@gmail.com>
2022-05-06 13:45:17 +0200

    I modified and removed some parts in these files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from "./Components/Layouts/Header/Header";
+import Cards from "./Components/Cards/Cards";
+import Footer from './Components/Layouts/Footer/Footer';
+import { GOTProvider } from './Contexts/Context';
+import CharacterPage from './view/CharacterPage';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <GOTProvider>
+          <CharacterPage>
+              <Cards />
+          </CharacterPage>
+        </GOTProvider>
+      <Footer/>
     </div>
   );
 }
