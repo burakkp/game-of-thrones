0a97467cd10c5c55d7f777e26201f10a884015e5
Burak Kucukparmaksiz <burakkp@gmail.com>
2022-05-06 13:52:13 +0200

    I created Characterpage for demonstrate characters

---
+++ src/view/CharacterPage.js
@@ -0,0 +1,14 @@
+import Cards from "../Components/Cards/Cards";
+import Pagination from "../Components/Pagination/Pagination";
+
+const CharacterPage = () => {
+    
+        return (
+            <div>
+                <Cards />
+                <Pagination />
+            </div>
+    )
+}
+
+export default CharacterPage;
