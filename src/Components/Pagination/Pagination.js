2f596098eeac10f9009d9dc5df6e28d1bf9424ae
Burak Kucukparmaksiz <burakkp@gmail.com>
2022-05-09 23:45:19 +0200

    I created pagination component

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -1,36 +1,43 @@
 import React, { useContext } from 'react';
 import { GOTContext } from "../../Contexts/Context";
+import './Pagination.scss';
 
 const Pagination = () => {
-    //const [page, setPage] = useContext(GOTContext);
-    const maxPage = 214;
-
+    const {page, setPage, characters} = useContext(GOTContext);
+    
+    const maxPage = characters.length;
+    
     return (
         <nav className="pagination" role="navigation" aria-label="pagination">
-            <a className="pagination-previous">Previous</a>
-            <a className="pagination-next">Next page</a>
+            <button className="pagination-previous" rel='prev' disabled={page === 1} onClick={() => setPage(page-1) }>Previous</button>
+            <button className="pagination-next" rel='next' disabled={page === maxPage} onClick={() => setPage(page+1)}>Next page</button>
             <ul className="pagination-list">
                 <li>
-                    <a className="pagination-link" aria-label="Goto page 1">1</a>
+                    <button className={page === 1 ? "pagination-link is-current" : "pagination-link"} aria-label="Goto page 1" onClick={() => setPage(1)}>1</button>
                 </li>
-                <li>
-                    <span className="pagination-ellipsis">&hellip;</span>
-                </li>
-                <li>
-                    <a className="pagination-link" aria-label="Goto page 45">45</a>
-                </li>
-                <li>
-                    <a className="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a>
-                </li>
-                <li>
-                    <a className="pagination-link" aria-label="Goto page 47">47</a>
-                </li>
-                <li>
-                    <span className="pagination-ellipsis">&hellip;</span>
-                </li>
-                {/* <li>
-                    <a className="pagination-link" aria-label={"Goto page "+ maxPage}>{maxPage}</a>
-                </li> */}
+                {
+                    page > 2 ?
+                    <li>
+                        <span className="pagination-ellipsis">&hellip;</span>
+                    </li>
+                    : ''
+                }
+                {
+                    page !== 1 ?
+                    <li>
+                        <button className="pagination-link is-current" aria-label={"Page " + (page)} aria-current="page">{page}</button>
+                    </li>
+                    : ''
+                }
+                
+                {
+                    page !== maxPage ?
+                    <li>
+                        <button className="pagination-link" aria-label={"Goto page "+ (page+1)} onClick={() => setPage(page+1)}>{page+1}</button>
+                    </li>
+                    : ''
+                }
+                                
             </ul>
         </nav>
     )
