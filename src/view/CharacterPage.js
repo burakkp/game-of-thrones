8694bf8a936c2eac5a30060feca29334b9d8403e
Burak Kucukparmaksiz <burakkp@gmail.com>
2022-05-09 23:48:45 +0200

    I updated my character page

--- src/view/CharacterPage.js
+++ src/view/CharacterPage.js
@@ -1,13 +1,32 @@
-import Cards from "../Components/Cards/Cards";
+import { useContext } from "react";
+import Table from "../Components/Table/Table";
 import Pagination from "../Components/Pagination/Pagination";
+import { GOTContext } from "../Contexts/Context";
 
 const CharacterPage = () => {
+    const {characters} = useContext(GOTContext);
     
-        return (
-            <div>
-                <Cards />
-                <Pagination />
-            </div>
+    const columns = [
+        { accessor: 'name', label: 'Name' },
+        { accessor: 'gender', label: 'Gender' },
+        { accessor: 'age', label: 'Age' },
+        { accessor: 'culture', label: 'Culture' },
+        { accessor: 'born', label: 'Born' },
+        { accessor: 'died', label: 'Died' },
+        { accessor: 'father', label: 'Father', format: 'url' },
+        { accessor: 'mother', label: 'Mother', format: 'url' },
+        { accessor: 'spouse', label: 'Spouse', format: 'url' },
+        { accessor: 'titles', label: 'Titles' },
+        { accessor: 'aliases', label: 'Aliases' },
+      ]
+    
+      const rows = characters;
+
+    return (
+        <div>
+            <Table rows={rows} columns={columns} />
+            <Pagination />
+        </div>
     )
 }
 
