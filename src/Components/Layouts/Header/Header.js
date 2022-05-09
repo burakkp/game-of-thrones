37778bb4dd7b77c9094ab010eeb14362ecfcce97
Burak Kucukparmaksiz <burakkp@gmail.com>
2022-05-09 23:44:17 +0200

    I updated my app header

--- src/Components/Layouts/Header/Header.js
+++ src/Components/Layouts/Header/Header.js
@@ -14,35 +14,7 @@ const Header = () => {
 
                 <div id="navbarBasicExample" className="navbar-menu">
                     <div className="navbar-start">
-                    <button className="navbar-item">
-                        Home
-                    </button>
-
-                    <a className="navbar-item">
-                        Documentation
-                    </a>
-
-                    <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">
-                        More
-                        </a>
-
-                        <div className="navbar-dropdown">
-                        <a className="navbar-item">
-                            About
-                        </a>
-                        <a className="navbar-item">
-                            Jobs
-                        </a>
-                        <a className="navbar-item">
-                            Contact
-                        </a>
-                        <hr className="navbar-divider" />
-                        <a className="navbar-item">
-                            Report an issue
-                        </a>
-                        </div>
-                    </div>
+                    
                     </div>
                     <div>
                     <a className="navbar-item" href="/">
@@ -52,14 +24,7 @@ const Header = () => {
 
                     <div className="navbar-end">
                     <div className="navbar-item">
-                        <div className="buttons">
-                        <a className="button is-primary">
-                            <strong>Sign up</strong>
-                        </a>
-                        <a className="button is-light">
-                            Log in
-                        </a>
-                        </div>
+                        
                     </div>
                     </div>
                 </div>
